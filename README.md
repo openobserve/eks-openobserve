ff5f824232c93403070acb5772679b3e86160203
Prabhat Sharma <hi.prabhat@gmail.com>
2024-08-25 17:37:04 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # eks-openobserve
+
 Guide on installing OpenObserve on Amazon EKS
+
+## Install eksctl and create an EKS cluster (Skip if EKS cluster already exists)
+
+Installation guide for eksctl at: https://eksctl.io/installation/
+
+Create EKS cluster
+
+> eksctl create cluster -f o2-eks.yaml
+
+Cluster creation started at 1:42 PM
+
+## Install Helm (If not already installed)
+
+Follow instructions at https://helm.sh/docs/intro/install/
+
+## Create s3 Bucket and IAM Policy, Role for OpenObserve
+
+
+
+## Download values.yaml file
+
+```shell
+wget https://raw.githubusercontent.com/openobserve/openobserve-helm-chart/main/charts/openobserve/values.yaml
+```
+
+## Update values.yaml file
+
+1. Update the `serviceAccount` section with the ARN of the IAM role created in the previous step
+2. Update the bucket name
+3. Update the credentials
+
+## Install OpenObserve
+
+### Setup prerequisites
+
+Install CloudNativePG operator (Use this if you are not using Amazon RDS. RDS is highly recommended due to its easy management and maintenance)
+
+```shell
+
+kubectl apply --server-side -f \
+  https://raw.githubusercontent.com/cloudnative-pg/cloudnative-pg/main/releases/cnpg-1.24.0.yaml
+```
+
+### Add OpenObserve Helm repository
+
+```shell
+helm repo add openobserve https://openobserve.github.io/openobserve-helm-chart
+
+kubectl create ns openobserve
+
+helm --namespace openobserve -f values.yaml install o2 openobserve/openobserve
+```
---
+++ bucket.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# Set the cluster name
+CLUSTER_NAME=o2
+NAMESPACE=openobserve
+SERVICE_ACCOUNT=openobserve
+
+# Get the AWS account ID
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+
+# Get the AWS region
+AWS_REGION=$(aws eks describe-cluster --name $CLUSTER_NAME --query 'cluster.arn' --output text | cut -d: -f4)
+
+# Get the OIDC ID
+OIDC_ID=$(aws eks describe-cluster --name $CLUSTER_NAME --query 'cluster.identity.oidc.issuer' --output text | cut -d/ -f5)
+
+# Generate random 5 digit number
+random_number=$((RANDOM%90000+10000))
+bucket="openobserve-$random_number"
+
+# Create s3 bucket for OpenObserve
+aws s3 mb s3://$bucket
+
+echo "S3 bucket created: $bucket"
+
+# Set the policy name and description
+POLICY_NAME="OpenObservePolicy"
+POLICY_DESCRIPTION="Policy for S3 access by OpenObserve"
+
+# Set the policy document
+POLICY_DOCUMENT='{
+  "Id": "Policy1678319681097",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Stmt1678319677242",
+      "Action": [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:DeleteObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::'$bucket'/*"
+    }
+  ]
+}'
+
+# Create the policy
+aws iam create-policy --policy-name $POLICY_NAME --policy-document "$POLICY_DOCUMENT" --description "$POLICY_DESCRIPTION"
+
+# Set the role name and description
+ROLE_NAME="OpenObserveRole"
+ROLE_DESCRIPTION="Role for EKS service account"
+
+# Create the role
+aws iam create-role --role-name $ROLE_NAME --description "$ROLE_DESCRIPTION" --assume-role-policy-document '{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "arn:aws:iam::'$AWS_ACCOUNT_ID':oidc-provider/oidc.eks.'$AWS_REGION'.amazonaws.com/id/'$OIDC_ID'"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringLike": {
+          "oidc.eks.'$AWS_REGION'.amazonaws.com/id/'$OIDC_ID':aud": "sts.amazonaws.com"
+        }
+      }
+    }
+  ]
+}'
+
+# Attach the policy to the role
+aws iam attach-role-policy --role-name $ROLE_NAME --policy-arn arn:aws:iam::'$AWS_ACCOUNT_ID':policy/$POLICY_NAME
+
+echo "Role created: $ROLE_NAME"
